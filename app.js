afe79a851a2dc2a243b926a07d3a66788d37f9fb
Ana Ortega <anaa0hh@icloud.com>
2018-02-17 13:58:42 -0800

    graph displaying results correctly

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 Picture.allPictures = [];
 var totalClicks = 0;
 
+
 // console.log(Picture.allPictures);
 
 
@@ -129,6 +130,7 @@ function randomIndexes(htmlObject) {
     imgEl2.removeEventListener('click', generatePictures);
     imgEl3.removeEventListener('click', generatePictures); 
     showTally();
+    makeChart();
   }
 
 }
@@ -136,40 +138,40 @@ randomIndexes();
 
 
 //////////////////////////////////////////CHART IS BELOW///////////////////////////////////////////////
-var ctx = document.getElementById('myChart');
-var myChart = new Chart(ctx, {
-  type: 'bar',
-  data: {
-    labels: [],
-    datasets: [{
-      label: '# of Votes',
-      data: [12, 19, 3, 5, 2, 3],
-      backgroundColor: [
-        'rgba(255, 99, 132, 0.2)',
-        'rgba(54, 162, 235, 0.2)',
-        'rgba(255, 206, 86, 0.2)',
-        'rgba(75, 192, 192, 0.2)',
-        'rgba(153, 102, 255, 0.2)',
-        'rgba(255, 159, 64, 0.2)'
-      ],
-      borderColor: [
-        'rgba(255,99,132,1)',
-        'rgba(54, 162, 235, 1)',
-        'rgba(255, 206, 86, 1)',
-        'rgba(75, 192, 192, 1)',
-        'rgba(153, 102, 255, 1)',
-        'rgba(255, 159, 64, 1)'
-      ],
-      borderWidth: 1
-    }]
-  },
-  options: {
-    scales: {
-      yAxes: [{
-        ticks: {
-          beginAtZero:true
-        }
-      }]
-    }
+function makeChart() {
+  var numClicks =[];
+  var products = [];
+  var numViews = [];
+
+  for(var i = 0; i < Picture.allPictures.length; i++){
+    numClicks.push(Picture.allPictures[i].pictureClicked);
+    products.push(Picture.allPictures[i].name);
+    numViews.push(Picture.allPictures[i].pictureShown);
   }
-});
+
+  var ctx = document.getElementById('myChart');
+  var myChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+      labels: products,
+      datasets: [{
+        label: '# of Clicks',
+        data: numClicks,
+        backgroundColor: 'purple',
+      },
+      { label: '# of Times Shown',
+        data: numViews,
+        backgroundColor: 'green',
+      }]
+    },
+    options: {
+      scales: {
+        yAxes: [{
+          ticks: {
+            beginAtZero:true
+          },
+        }]
+      }
+    }
+  });
+}
